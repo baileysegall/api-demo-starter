ea5fbe95ad1ed593f7cd217c4515bdbf76ade279
Rebecca Dang <ph.rdang@gmail.com>
2021-10-23 10:50:34 -0700

    Format App.js with Prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-//Necessary import statements 
-import './App.css';
+//Necessary import statements
+import "./App.css";
 import axios from "axios";
-import React, { useState }  from 'react';
+import React, { useState } from "react";
 
 //The App function is the main output function which all the main code is stored in
 // in a more complex application may have multiple components used together
@@ -12,27 +12,26 @@ function App() {
 
   //Handle getting more info from api for the hover
 
-
   //If doggo is not updated (the button has not been clicked) return no image
-  if(true){ //replace
+  if (true) {
+    //replace
     return (
       <div class="main">
         <p class="title">CODEBASE API DEMO</p>
         {/* when button is called called url function */}
-        <button  class="button" >Gimme Doggies</button>
+        <button class="button">Gimme Doggies</button>
       </div>
-  );}
+    );
+  }
   //if the doggo is updated render the image and the hover component
   else {
     return (
       <div class="main">
         <p class="title-with-image">CODEBASE API DEMO</p>
-        <div class="container">
-          {/* Render image from api */}
-        </div>
-        <button class="button-with-image" >Gimme Doggies</button>
+        <div class="container">{/* Render image from api */}</div>
+        <button class="button-with-image">Gimme Doggies</button>
       </div>
-  );
+    );
   }
 }
 //exports the app
